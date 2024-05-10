9c8c1166ddce7089b2b2ef8b75ce130673fa824e
kimkinghyeon <modan8199@gmail.com>
2024-05-10 14:20:52 +0900

    literal Applicatoin1

---
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section1/literal/Application1.java
@@ -0,0 +1,48 @@
+package com.ohgiraffers.section1.literal;
+
+public class Application1 {
+    public static void main(String[] args) {
+    /*
+    System.out.print("a");
+    System.out.print("a");
+    System.out.println("b");
+    System.out.println("c");
+
+    System.out.println("편해요 ㅎㅎ");
+    System.out.println();
+    */
+
+   /*
+    * 여기 사이에는 모든게 주석이 된다.
+    */
+
+    // 수업목표: 여러가지 값의 형태를 출력 할 수 있다.
+
+    System.out.println(12.3);
+    System.out.println(123);
+
+    // 문자 출력
+    System.out.println('a');// 문자 형태의값은 홀따옴표로 표시
+//      System.out.println('ab');
+
+    System.out.println('1'); // 숫자 값이지만 홀따옴표로 표시해서 문자 '1'로 판단
+
+
+    // 문자 출력
+    System.out.println("안녕하세요"); // 문자열은 문자가 여러개가 나열된 형태를 의미하며 쌍따옴표로 표시
+    System.out.println("123"); // 정수이지만 쌍따옴표로 표시했기에 문자열로 보아야한다.
+    System.out.println(""); // 아무 값도 없는 빈쌍따옴표도 문자열로 취급된다. (문자는 불가능)
+    System.out.println("a"); // 한개의 문자도 쌍따옴표로 표시하면 문자열이다
+
+    // 논리값 출력
+    System.out.println(true);
+    System.out.println(false);
+
+
+
+
+
+
+
+     }
+}
