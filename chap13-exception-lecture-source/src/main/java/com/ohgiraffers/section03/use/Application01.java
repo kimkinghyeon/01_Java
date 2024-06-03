9c9272ffeb090bce8dfe01892f622b578a7a7998
kimkinghyeon <modan8199@gmail.com>
2024-06-03 15:24:27 +0900

    수업내용 수정 불필요한 프로젝트 삭제 수업내용 :Enum

--- chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section03/use/Application01.java
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section03/use/Application01.java
@@ -1,33 +1,29 @@
 package com.ohgiraffers.section03.use;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Application01 {
     // io 패키지에서 예외처리
     public static void main(String[] args) {
-        BufferedReader in = null;
+        BufferedReader br = null;
 
         try {
-<<<<<<< HEAD
+
 
             // FileReader라는 클래스에 FileNotFoundExcepiton을
             // throw 해놓아서 try-catch로 예외처리를 해주어야함.
-=======
+
             // FileReader라는 클래스에 FileNotFoundExcepiton을
->>>>>>> 0690f96f7a439dfb3ef048529508f3b047fd2530
-            in = new BufferedReader(new FileReader("test.dat"));
+
+            br = new BufferedReader(new FileReader("test.dat"));
 
             // 파일을 읽어서 저장할 문자열
             String s;
 
             // 파일을 한줄씩 읽어서 출력하는 기능
-<<<<<<< HEAD
-            while ((s = in.readLine()) != null) {
-=======
->>>>>>> 0690f96f7a439dfb3ef048529508f3b047fd2530
+
+            while ((s = br.readLine()) != null) {
+
                 System.out.println(s);
             }
 
@@ -39,41 +35,36 @@ public class Application01 {
 
             // IOException
             // In - Out 예외클래스
-<<<<<<< HEAD
+
             // catch 블럭은 여러개 작성할 때, 상위 타입이 하단에오고, 후손 타입이 상단에 온다.
-=======
->>>>>>> 0690f96f7a439dfb3ef048529508f3b047fd2530
+
             throw new RuntimeException(e);
         } finally {
             /*
              * finally
              *
-<<<<<<< HEAD
              * 예외 처리 구문과 상관 없이 반드시 수행하는 경우 작성한다.
              * 보통 사용한 자원을 반납할 목적으로 사용된다.
              * */
-=======
->>>>>>> 0690f96f7a439dfb3ef048529508f3b047fd2530
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
             try {
+                bw.write("java mysql jdbc");
 
                 // BufferReader 객체가 없을 때를 대비해서 조건문으로 작성
-<<<<<<< HEAD
-                if(in != null) {
+                // 자원 반환
 
-                    // 자원 반환
-=======
-                if (in != null) {
-
->>>>>>> 0690f96f7a439dfb3ef048529508f3b047fd2530
-                    in.close();
-                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            } finally {
+                if (bw != null) {
+                    try {
+                        bw.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         }
-
-
-
     }
 }
---
+++ chap15-enum-lecture-source/src/main/java/com/ohgiraffers/section01/intenum/Application.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.section01.intenum;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* Enum(열거타입)
+        * 제한된 값 목록이 필요한경우 사용한다.
+        *
+        * Enum(이넘)이란 관련이 있는 상수의 집합의 클래스를 의미한다.
+        * 각각의 열거 상수는 열거 객체로 생성된다.
+        * 기존의 상수를 정의하는 방법을 효과적으로 대체한다.
+        *
+        * 기존 정수 열거 패턴의 단점
+        * 1. 정수형 상수는 의미가 있는 문자열로 출력하기 어렵다.
+        * 2. 정수 열거 그룹에 속한 상수들을 순회 할 수 없다. (갯수 확인도 불가능)
+        * 3. 타입의 안전을 보장 할 수 없다.*/
+
+        int subject1 = Subjects.JAVA; // 0
+        int subject2 = Subjects.JAVA; // 0
+
+        if(subject1 == subject2){
+            System.out.println("두과목은 같은 과목입니다.");
+        }
+
+        // 2. 문자열로 출력하기가 까다롭다.
+        int num = 0;
+        String subjectText = "";
+
+        switch (num){
+            case Subjects.JAVA: subjectText = "JAVA"; break;
+            case Subjects.JDBC: subjectText = "JDBC"; break;
+            case Subjects.ORACLE: subjectText = "ORACLE"; break;
+        }
+        System.out.println("subjectText = " + subjectText);
+        System.out.println("subjectText = " + subjectText);
+        System.out.println("subjectText = " + subjectText);
+    }
+
+}
